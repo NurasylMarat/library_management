5385509e651ed75a046865a14673c2e266c5476e
Nur <nurasylmarat07@gmail.com>
2026-01-21 10:15:22 +0500

    Initial commit

--- src/Library/BookDAO.java
+++ src/Library/BookDAO.java
@@ -6,56 +6,68 @@ import java.util.List;
 
 public class BookDAO {
 
-    public void addBook (Book book) throws SQLException {
+    public void addBook (Book book) {
         String sql = "INSERT INTO books(title, year) VALUES (?, ?)";
 
-        Connection conn = DatabaseConnection.getConnection();
-        PreparedStatement stmt = conn.prepareStatement(sql);
+        try (Connection conn = DatabaseConnection.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
 
-        stmt.setString(1, book.getTitle());
-        stmt.setInt(2, book.getYear());
-        stmt.executeUpdate();
-
-        conn.close();
+            stmt.setString(1, book.getTitle());
+            stmt.setInt(2, book.getYear());
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
 
-    public List<Book> getAllBooks() throws SQLException {
+    public List<Book> getAllBooks() {
         List<Book> books = new ArrayList<>();
         String sql = "SELECT title, year FROM books";
 
-        Connection conn = DatabaseConnection.getConnection();
-        Statement stmt = conn.createStatement();
-        ResultSet rs = stmt.executeQuery(sql);
+        try (Connection conn = DatabaseConnection.getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
 
-        while (rs.next()) {
-            String title = rs.getString("title");
-            int year = rs.getInt("year");
+            while (rs.next()) {
+                String title = rs.getString("title");
+                int year = rs.getInt("year");
             }
 
-        return books;
+        } catch (SQLException e){
+            e.printStackTrace();
         }
+        return books;
+    }
 
 
-    public void updateYear(String title, int newYear) throws SQLException {
+    public void updateYear(String title, int newYear) {
         String sql = "UPDATE books SET year = ? WHERE title = ?";
 
-        Connection conn = DatabaseConnection.getConnection();
-        PreparedStatement stmt = conn.prepareStatement(sql);
+        try (Connection conn = DatabaseConnection.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
 
-        stmt.setInt(1, newYear);
-        stmt.setString(2, title);
-        stmt.executeUpdate();
+                stmt.setInt(1, newYear);
+                stmt.setString(2, title);
+                stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
+    }
 
 
-    public void deleteBook(String title) throws SQLException {
+    public void deleteBook(String title) {
         String sql = "DELETE FROM books WHERE title = ?";
 
-        Connection conn = DatabaseConnection.getConnection();
-        PreparedStatement stmt = conn.prepareStatement(sql);
+        try (Connection conn = DatabaseConnection.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
 
-        stmt.setString(1, title);
-        stmt.executeUpdate();
+            stmt.setString(1, title);
+            stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
